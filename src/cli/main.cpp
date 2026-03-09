2781a39f8a37d0162ef79b5481f8ae2015e6c7ad
Alexandre Alvaro <alexandre.alvaro@hotmail.com>
2026-03-08 23:27:03 -0300

    chore: initialize library and cli build structure

---
+++ src/cli/main.cpp
@@ -0,0 +1,13 @@
+# CLI entry point skeleton
+# This will be refined as the library evolves.
+
+#include <corridorkey/api_export.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+    
+    std::cout << "CorridorKey Runtime CLI (Skeleton)" << std::endl;
+    return 0;
+}
