a4f15d4e398fa7c634e9f2f4c6a75d90e86a7773
Alexandre Alvaro <alexandre.alvaro@hotmail.com>
2026-03-08 23:27:03 -0300

    feat: add api_export macro for symbol visibility control

---
+++ include/corridorkey/api_export.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * @file api_export.hpp
+ * @brief Macros for managing symbol visibility and library export/import.
+ */
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #ifdef CORRIDORKEY_EXPORTS
+        #define CORRIDORKEY_API __declspec(dllexport)
+    #else
+        #define CORRIDORKEY_API __declspec(dllimport)
+    #endif
+#else
+    #if __GNUC__ >= 4
+        #define CORRIDORKEY_API __attribute__ ((visibility ("default")))
+    #else
+        #define CORRIDORKEY_API
+    #endif
+#endif
