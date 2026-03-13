82614bd04a62c126ca0f90a8303c16e7caa8a443
alexandremendoncaalvaro <alexandre.alvaro@hotmail.com>
2026-03-12 23:49:02 -0300

    feat: integrate tauri-plugin-shell for backend-to-frontend communication

--- src/gui/src-tauri/src/lib.rs
+++ src/gui/src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
